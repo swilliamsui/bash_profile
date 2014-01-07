8b304b3cc89ba3c639fc02b976b959155708d7b7
swilliams <stephen@us.sendgine.com>
2014-01-07 14:25:11 -0500

    include one-and-done file to add one line to .profile

---
+++ swilliamsui-bash-profile-sources.sh
@@ -0,0 +1,11 @@
+# extend profile
+source ~/.profile_dir/path.sh
+source ~/.profile_dir/aliases.sh
+source ~/.profile_dir/bookmarks.sh
+source ~/.profile_dir/prompt.sh
+
+source ~/.profile_dir/aws.sh
+
+source ~/.profile_dir/git-alias.sh
+source ~/.profile_dir/git-prompt.sh
+source ~/.profile_dir/git-completion.sh
